64b19bf3fe1d4d0377d445059287ec58775df835
Keith Dahlby <keith@newbo.co>
2021-05-11 10:25:06 -0500

    Style our list as toast

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,3 +1,6 @@
+import Badge from 'react-bootstrap/Badge';
+import Toast from 'react-bootstrap/Toast';
+
 const list = [
     {
         "id": 37,
@@ -44,7 +47,21 @@ function ToDoItem(props)
 {
     const { todo } = props;
 
+    const pillText = todo.completed ? 'Complete' : 'Pending';
+    const pillVariant = todo.completed ? 'danger' : 'success';
+
     return (
-        <h3>{todo.title}</h3>
+        <Toast>
+            <Toast.Header>
+                <Badge pill variant={pillVariant}>{pillText}</Badge>
+                <strong class="ml-2 mr-auto">{todo.assignedTo}</strong>
+            </Toast.Header>
+            <Toast.Body>
+                {todo.title}
+                <div className="text-right">
+                    <small>Difficulty: {todo.difficulty}</small>
+                </div>
+            </Toast.Body>
+        </Toast>
     )
 }
